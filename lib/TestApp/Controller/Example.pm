2a50306d0e050e77d87814aafa9f4cf7c0ef71cf
Селютин Дмитрий <selutindmitrij@Mac-mini-manager.local>
2015-12-11 14:23:45 +0300

    correct version2

--- lib/TestApp/Controller/Example.pm
+++ lib/TestApp/Controller/Example.pm
@@ -6,30 +6,28 @@ use Data::Dumper;
 my $clients = {};
 
 sub buttonColor {
-	my $c = shift;
-	$c->render(template => 'example/button');
+  	my $self = shift;
+  	$self->render(template => 'example/button');
 };
 
 sub squareColor {	
-	my $c = shift;
-	$c->render(template => 'example/square');};
+  	my $self = shift;
+  	$self->render(template => 'example/square');};
 
 sub socket {
-
-  my $c = shift;
-  my $id = sprintf "%s", $c->tx;
-  print Dumper($id);
-  $clients->{$id} = $c->tx;
-
-  $c->on(json => sub {
-      my ($c, $hash) = @_;
-      $hash->{msg} = "echo: $hash->{msg}";
-      $c->send({json => $hash});
-      for (keys %$clients) {
-          $clients->{$_}->send({json => $hash});
-      }
-  }
-  );
+    my $self = shift;
+    my $id = sprintf "%s", $self->tx;
+    print Dumper($id);
+    $clients->{$id} = $self->tx;
+    $self->on(json => sub {
+        my ($self, $hash) = @_;
+        $hash->{msg} = "echo: $hash->{msg}";
+        $self->send({json => $hash});
+        for (keys %$clients) {
+            $clients->{$_}->send({json => $hash});
+        }
+    }
+    );
 };
 
 1;
